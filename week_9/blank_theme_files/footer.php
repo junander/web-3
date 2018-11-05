c2abe7b5d5dc90fda10609e171ffc7fca94bf0ae
Joe Unander <joe@early-adopter.com>
2018-11-05 17:35:56 -0500

    Week 9 files

---
+++ week_9/blank_theme_files/footer.php
@@ -0,0 +1,24 @@
+<!-- footer -->
+<footer class="footer" role="contentinfo">
+
+	<!-- copyright -->
+	<p class="copyright">
+		&copy;
+		<?php echo date('Y'); ?> Copyright
+		<?php bloginfo('name'); ?>.
+		<?php _e('Powered by', 'web3'); ?>
+		<a href="//wordpress.org" title="WordPress">Cats</a>.
+	</p>
+	<!-- /copyright -->
+
+</footer>
+<!-- /footer -->
+
+</div>
+<!-- /wrapper -->
+
+<?php wp_footer(); ?>
+
+</body>
+
+</html>
