df6089e86a9ac4edb5057b51ead4b91541938800
Joe Unander <joe@early-adopter.com>
2019-09-14 02:34:38 -0400

    Fall 2019: week 3 - homework updates

--- week_3/homework/index.php
+++ week_3/homework/index.php
@@ -1,11 +1,22 @@
 <?php
 
 /* 
- * Homework for Web 3: Week 2
+ * Homework for Web 3: Week 3
  */
 
+?>
+
+<?php
 /**
- * Assignment 1: take the file called header.php and include it in both the includes.php and more_includes.php files. Then change something or add something in header.php, and see how that shows up on both pages.
+ * Assignment 1 - variables: 1) In the file variables.php, there are two variables: $x and $y. Make an echo statement that shows the number 22, except you must use *both* x and y to create that number.
+ * There are also two additional variables, $a and $b. Make another echo statement that shows the word "twentytwo", except you must use *both* a and b to create that word.
+ * **Bonus** - change "twentytwo" to "twenty two" or "twenty-two"
+ */
+?>
+
+<?php
+/**
+ * Assignment 2 - includes: take the file called header.php and include it in both the includes.php and more_includes.php files. Then change something or add something in header.php, and see how that shows up on both pages.
  * Next create a new file called footer.php - and add some things to it (hint: I would recommend at least a closing body tag and closing html tag). Then include that footer.php in both includes.php and more_includes.php.
  * 
  */
@@ -16,7 +27,7 @@
 
 <?php
 /**
- * Assignment 2: go to conditionals.php. There you will find a basic condition that compares a numeric value to a condition. In this case, the numeric value is 2, and the condition asks if the value is greater than 3 - so right now this condition evaluates to false.
+ * Assignment 3 - conditionals: go to conditionals.php. There you will find a basic condition that compares a numeric value to a condition. In this case, the numeric value is 2, and the condition asks if the value is greater than 3 - so right now this condition evaluates to false.
  * Do three things here: 1) add a condition to check if the value equals 2, and add an appropriate echo statement (i.e. "This value equals 2").
  * 2) add a condition to check if the value is *less than* 2, and add an appropriate echo statement
  * 3) Test your conditions by changing the numeric value to different numbers
@@ -27,7 +38,7 @@
 
 <?php
 /**
- * Assignment 3: go to arrays.php, create an associative array, and then loop through it, printing out both the key and value
+ * Assignment 4 - arrays: go to arrays.php, create an associative array, and then loop through it, printing out both the key and value
  * This array will be made up of address values
  * Hint: 'Street' => '123 Street'
  * After you create the values, loop through the array and print out each key and value so that it looks like you are printing out
@@ -39,7 +50,7 @@
 
 <?php
 /**
- * Assignment 4: taking your array from Assignment 3, create a function that prints out a given address value based on the key
+ * Assignment 5 - functions: taking your array from Assignment 3, create a function that prints out a given address value based on the key
  * Hint: this will involve passing a variable to the function declaration
  * For example, I want to be able to ask my function for the street, and get '123 Street' back
  */
